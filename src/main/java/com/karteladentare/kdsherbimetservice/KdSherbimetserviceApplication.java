bde1ef0dda73116c150e74282a05492e53fe6686
QendresaZ <qz40092@ubt-uni.net>
2019-05-09 13:11:45 +0200

    first commit

---
+++ src/main/java/com/karteladentare/kdsherbimetservice/KdSherbimetserviceApplication.java
@@ -0,0 +1,13 @@
+package com.karteladentare.kdsherbimetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdSherbimetserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KdSherbimetserviceApplication.class, args);
+    }
+
+}
