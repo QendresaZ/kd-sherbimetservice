fc27c93789fe689ae3c63549138d2e250211b4b3
QendresaZ <qz40092@ubt-uni.net>
2019-05-09 14:55:45 +0200

    SherbimiRepository added

---
+++ src/main/java/com/karteladentare/kdsherbimetservice/repository/SherbimiRepository.java
@@ -0,0 +1,7 @@
+package com.karteladentare.kdsherbimetservice.repository;
+
+import com.karteladentare.kdsherbimetservice.domain.Sherbimi;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SherbimiRepository extends JpaRepository<Sherbimi, Long> {
+}
